5923527127e1e5ae46fb4f3bad660fdf175d5962
AmMilu <hehu@tcd.ie>
2019-09-23 19:41:22 +0100

    lca code done, test is the next

--- LCA/src/LCA.java
+++ LCA/src/LCA.java
@@ -1,9 +1,41 @@
+/*
+	To implement a function that calculate the lowest common ancestor in a graph,
+	that may be structured as a binary tree. 
+*/
+class Node{
+	int data;
+	Node left, right;
+	
+	Node(int num){
+		data = num;
+		left = null;
+		right = null;
+	}
+}
 
 public class LCA {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	Node root;
+	
+	Node lca(Node node, int a, int b) {
+		
+		if(node == null) {
+			return null;
+		}
+		
+		//a<root.data & b<root.data, which means the lca is in the left branch
+		if(a<node.data && b<node.data) {
+			return lca(node.left, a, b);
+		}
+		
+		//a>root.data & b>root.data, which means the lca is in the right branch
+		if(a>node.data && b>node.data) {
+			return lca(node.right, a, b);
+		}
+		
+		//if a<node.data && b>node.data, then the current node is the lca
+		//if a or b is equal to node.data, then the current node is the lca
+		return node;
 	}
 
 }
